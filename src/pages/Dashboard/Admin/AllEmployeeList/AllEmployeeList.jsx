a287f855700af1261802c094612c3be47349dd6c
arbadsha00 <arbadsha00@gmail.com>
2025-01-25 19:09:44 +0600

    make hr

--- src/pages/Dashboard/Admin/AllEmployeeList/AllEmployeeList.jsx
+++ src/pages/Dashboard/Admin/AllEmployeeList/AllEmployeeList.jsx
@@ -1,7 +1,105 @@
-import React from "react";
-
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import AuthContext from "@/provider/AuthContext";
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { IoGridSharp } from "react-icons/io5";
+import { FaTableList } from "react-icons/fa6";
+import { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { toast } from "react-toastify";
 const AllEmployeeList = () => {
-  return <div>AllEmployeeList</div>;
+  const { user } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: allEmployees = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["allEmployees", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/users`);
+      return data;
+    },
+  });
+  const [isCardView, setIsCardView] = useState(false);
+  const handleHR = async (id) => {
+    const result = await axiosSecure.patch(`/users/hr/${id}`);
+    if (result.data.modifiedCount) {
+      toast.success("Promoted to HR.");
+      refetch();
+    }
+  };
+  return (
+    <div>
+      <h3 className="text-primary-2 bg-sec text-center mb-3 text-2xl md:text-3xl font-bold">
+        All Employees List
+      </h3>
+      <div className="flex items-center justify-center">
+        <Button
+          onClick={() => setIsCardView(false)}
+          className={`rounded-r-none ${
+            isCardView ? "bg-primary-2" : "bg-secondary-1"
+          }`}
+        >
+          <FaTableList />
+        </Button>
+        <Button
+          onClick={() => setIsCardView(true)}
+          className={`rounded-l-none ${
+            isCardView ? "bg-secondary-1" : " bg-primary-2"
+          }`}
+        >
+          <IoGridSharp />
+        </Button>
+      </div>
+      <Separator className="my-4" />
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Designation</TableHead>
+            <TableHead>Salary</TableHead>
+            <TableHead>Update Salary</TableHead>
+            <TableHead>Make HR</TableHead>
+            <TableHead>Fire</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {allEmployees.map((employee) => (
+            <TableRow key={employee._id}>
+              <TableCell className="font-medium">{employee.name}</TableCell>
+              <TableCell>{employee.designation}</TableCell>
+              <TableCell>${employee.salary}</TableCell>
+              <TableCell>
+                <Button className="text-xs bg-secondary-1">Update</Button>
+              </TableCell>
+              <TableCell>
+                <Button
+                  onClick={() => handleHR(employee._id)}
+                  disabled={employee.role === "hr"}
+                  className="text-xs bg-primary-1"
+                >
+                  {employee.role === "hr" ? "HR" : "Make HR"}
+                </Button>
+              </TableCell>
+              <TableCell>
+                <Button className="text-xs bg-red-500">Fire</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default AllEmployeeList;
